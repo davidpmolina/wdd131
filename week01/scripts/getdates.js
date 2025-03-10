aa598ea394abbc09fb5be1c056bcd849a78d0a1d
davidpmolina <dmolina@byupathway.edu>
2025-03-10 09:51:17 +0800

    w01 Home Page completed

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,11 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Get the last modified date of the document
+const lastModified = document.lastModified;
+
+// Output the current year to the copyright span
+document.getElementById("currentyear").textContent = currentYear;
+
+// Output the last modified date to the lastModified paragraph
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
